fc8f9cd66298abd0f95c6e82045b55838f60d175
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-12-14 07:59:23 -0500

    got routes to notes and index html

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,5 +1,26 @@
 const fs = require("fs");
-const app = require("express");
+const express = require("express");
 const axios = require("axios");
 const path = require("path");
 
+const PORT = 3650;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../notes.html"));
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../index.html"));
+});
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+  
