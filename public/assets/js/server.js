4adbe3dd792f1d70527bb71ea17db8e4f33db71f
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-12-14 08:51:19 -0500

    get and post route complete, working on delete

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
-
+const db = require("../../../db/db.json")
 const PORT = 3650;
 const app = express();
 
@@ -17,8 +17,25 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../../index.html"));
 });
 
+app.get("/api/notes", function(req, res) {
+    fs.readFile(`${db}`, function(err, data) {
+        if (err) throw err;
+        res.json(db);
+    });
+});
 
+app.post("/api/notes", function(req, res) {
+    const newEntry = req.body;
+    fs.appendFile(`${db}`, newEntry, function(err, data) {
+        if (err) throw err;
+    });
+});
 
+app.delete("/api/notes/:id", function(req, res) {
+    fs.readFile(`${db}`, function(err, data) {
+
+    });
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
