acc2ca1d46ab2da6f92ea0d0a9e9137f0a8d17ce
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-12-18 14:53:09 -0500

    fixing file locations, working on heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
-const db = require("../../../db/db.json")
+const db = require("./db/db.json")
 const PORT = process.env.PORT || 3650;
 const app = express();
 
@@ -10,11 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/api/notes", function(req, res) {
