2ca3d7626dfa15bcb5d9b32026ad86a6ef7d7111
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-12-18 15:29:01 -0500

    fixed routes and heroku deploy

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Notetaker
-    Notetaker is an app that allows users to views previously made notes, add additional notes and delete notes.
+Notetaker is an app that allows users to views previously made notes, add additional notes and delete notes.
 ## Motivation
-    I wanted to become more familiar with express routing and managing data throught CRUD requests.
+I wanted to become more familiar with express routing and managing data throught CRUD requests.
 ## What I Learned
-    I learned more about express npm and deploying apps to heroku
+I learned more about express npm and deploying apps to heroku
 ## Link
-    https://infinite-dusk-40735.herokuapp.com/
+[Deploy site](https://infinite-dusk-40735.herokuapp.com/)
+## Screen Shot
+![Screen shot](https://i.imgur.com/rSM8g4Q.png)
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
-const db = require("./db/db.json")
+const db = "./db/db.json"
 const PORT = process.env.PORT || 3650;
 const app = express();
-
+app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,22 +18,26 @@ app.get("/", function(req, res) {
 });
 
 app.get("/api/notes", function(req, res) {
-    fs.readFile(`${db}`, function(err, data) {
+    fs.readFile(db, "utf8", function(err, data) {
         if (err) throw err;
-        res.json(db);
+        res.json(JSON.parse(data));
     });
 });
 
 app.post("/api/notes", function(req, res) {
-    const newEntry = req.body;
-    fs.appendFile(`${db}`, newEntry, function(err, data) {
-        if (err) throw err;
+    fs.readFile(db, "utf8", function(err, data){
+        const table = JSON.parse(data);
+        table.push(req.body);
+        fs.writeFile(db, JSON.stringify(table), function(err){
+            if (err) return res.json({success: false, error: err})
+            res.json({success: true})
+        });
     });
 });
 
 app.delete("/api/notes/:id", function(req, res) {
-    fs.readFile(`${db}`, function(err, data) {
-
+    fs.readFile(db, function(err, data) {
+        
     });
 });
 
